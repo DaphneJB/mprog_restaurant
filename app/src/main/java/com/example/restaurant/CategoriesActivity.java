1088f1a3d3e712c73bd13cf8417df7a49f86593f
Daphne JB <d.v.jonkersboth@amc.uva.nl>
2019-03-12 18:02:29 +0100

    Added categoriesrequest class

--- app/src/main/java/com/example/restaurant/CategoriesActivity.java
+++ app/src/main/java/com/example/restaurant/CategoriesActivity.java
@@ -2,12 +2,28 @@ package com.example.restaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class CategoriesActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class CategoriesActivity extends AppCompatActivity implements CategoriesRequest.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+        CategoriesRequest req = new CategoriesRequest(this);
+        req.getCategories(this);
+        Toast.makeText(this, "started", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void gotCategories(ArrayList<String> categories) {
+        Toast.makeText(this, categories.get(0), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void gotCategoriesError(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/example/restaurant/CategoriesRequest.java
@@ -0,0 +1,57 @@
+package com.example.restaurant;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
+public class CategoriesRequest implements Response.Listener<JSONObject>, Response.ErrorListener {
+
+    private Context context;
+    private ArrayList<String>cat;
+    private JSONArray category;
+    private Callback activity;
+
+    public CategoriesRequest(Context cont) {
+        context = cont;
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        activity.gotCategoriesError(error.getMessage());
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        try {
+            category = response.getJSONArray("categories");
+            for(int i = 0; i < category.length(); i++) {
+                cat.add(category.getString(i));
+            }
+            activity.gotCategories(cat);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public interface Callback {
+        void gotCategories(ArrayList<String> categories);
+        void gotCategoriesError(String message);
+    }
+
+    public void getCategories(Callback activity) {
+        this.activity =activity;
+        RequestQueue queue = Volley.newRequestQueue(context);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("https://resto.mprog.nl/categories", null, this, this);
+        queue.add(jsonObjectRequest);
+    }
+}
